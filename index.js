3291d259ab0851a7020a46a9fa5ba360352bbbba
MD OWAIS ATHAR <owais.athar.8092@gmail.com>
2022-11-07 16:09:47 +0530

    Backed Deployment

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-//backend structure git repo creation
-
 const express  = require("express")
 const { connection } = require("./config/db")
 const { todoRouter } = require("./routes/Todo.routes")
 const { userRouter } = require("./routes/User.routes")
+const {TodoModel} = require("./models/TodoModel")
+const cors = require("cors")
+app.use(cors())
 const jwt = require("jsonwebtoken")
 const { authentication } = require("./middlewares/authentication")
 const app = express()
@@ -16,9 +17,32 @@ app.get("/", (req,res)=>{
     res.send("Welcome to Todo Application")
 })
 
-app.use(authentication)
+//app.use(authentication)
 app.use("/" , userRouter)
-app.use("/todos", todoRouter)
+// app.use("/todos", todoRouter)
+
+//GET TODOS 
+
+app.get("/todos", authentication, async(req,res)=>{
+    const {user_id} = req.body
+    const all_todos = await TodoModel.find({user_id : user_id})
+    res.send({"Todos" : all_todos})
+})
+
+//POST TODOS
+
+app.post("/create", authentication, async(req,res)=>{
+    const {taskname, status, tag , user_id} = req.body
+
+    const new_todos = new TodoModel({
+        taskname,
+        status,
+        tag,
+        user_id
+    })
+    await new_todos.save()
+    res.send({"message" : "Todo Created"})
+})
 
 app.listen(PORT, async(req,res)=>{
     try{
